5ec05ed515922c38c6747d1aea1e6e14c28a3f8a
SNIPE_SCOPE <84712110+pranav10780@users.noreply.github.com>
2023-07-02 08:54:58 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,13 @@
+/*calculator v1.0*/
+#include<stdio.h>
+int main()
+{
+int v1,v2,sum;
+printf("Enter first number:");
+scanf("%d",&v1);
+printf("Enter second number:");
+scanf("%d",&v2);
+sum = v1 + v2;
+printf("The sum of the number is %i\n",sum);
+}
+
